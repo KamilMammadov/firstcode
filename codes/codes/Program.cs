14cd382d31cbda8a71d334dd8521ee3ffe26bc67
unknown <kamiltm@code.edu.az>
2022-05-27 04:44:29 -0700

    firstcodes

---
+++ codes/codes/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace codes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x = 5;
+            int y = 7;
+
+            int a = x + y;
+
+            Console.WriteLine(a);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
